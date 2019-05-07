cd0e6cb3e6405ed49ccc65cf687c6d40466dbe91
Unknown <stelios.gia.sweden@gmail.com>
2019-05-07 22:41:33 +0200

    minor bug fix, src folder added

--- src/NotificationService/ExtrationAndProcessing/SendoutProcessing.cs
+++ src/NotificationService/ExtrationAndProcessing/SendoutProcessing.cs
@@ -21,7 +21,8 @@ namespace NotificationService.Processing
                 try
                 {
                     var userList = new List<User>();
-                    var template = templates.FirstOrDefault(x => x.NotificationName == sendout.ReminderName);
+                    var template = templates
+                                    .FirstOrDefault(x => x.NotificationName == sendout.ReminderName);
                     var user = users.FirstOrDefault(x => x.Username == sendout.Username);
                     var temp = userGroups
                         .FirstOrDefault(x => x.GroupName == sendout.UserGroup)
@@ -32,8 +33,9 @@ namespace NotificationService.Processing
                     if (user != null) userList.Add(user);
                     if (userGroup != null) userList.AddRange(userGroup);
                     var meanOfCommunication = ExtractPriority(template, userList);
-                    var listOfContactDetails = meanOfCommunication == MeansOfCommunication.Email ?
-                        userList.Select(x => x.Email) : userList.Select(x => x.SMS);
+                    var listOfContactDetails = meanOfCommunication == MeansOfCommunication.Email 
+                        ? userList.Select(x => x.Email) 
+                        : userList.Select(x => x.SMS);
 
                     sendoutReadyList.AddRange(listOfContactDetails.Select(x => new GatewaySendout()
                     {
@@ -53,16 +55,18 @@ namespace NotificationService.Processing
         private MeansOfCommunication ExtractPriority(Template template, IEnumerable<User> user)
         {
             if (Convert.ToInt16(template.NotificationPriority) == (int) MeansOfCommunication.Email &&
-               user.All(x => !String.IsNullOrEmpty(x.Email))) return MeansOfCommunication.Email;
+               user.All(x => !String.IsNullOrEmpty(x.Email)))
+                return MeansOfCommunication.Email;
             if (Convert.ToInt16(template.NotificationPriority) == (int) MeansOfCommunication.SMS &&
-                user.All(x => !String.IsNullOrEmpty(x.SMS))) return MeansOfCommunication.SMS;
+                user.All(x => !String.IsNullOrEmpty(x.SMS)))
+                return MeansOfCommunication.SMS;
 
             throw new ArgumentException("Not specific priority is set for this notification");
         }
 
         private string GetParsedNotificationText(Sendout sendout, Template template)
         {
-            var parameterSet = sendout.Parameters.Split(",").Select(x => x.Trim()).ToList();
+            var parameterSet = sendout.Parameters.Split(",").Select(x => x.Trim().ToLower()).ToList();
             var dictionaryParam = new Dictionary<string, string>();
             foreach (var line in parameterSet)
             {
@@ -78,11 +82,11 @@ namespace NotificationService.Processing
                     squareBracketsFound = false;
                     continue;
                 }
-                var wordWithBrackets = template.NotificationText.Substring(openBracketIndex, closeBracketIndex - openBracketIndex + 1);
-                template.NotificationText = dictionaryParam.ContainsKey(wordWithBrackets.Substring(1, wordWithBrackets.Length - 1)) ?
+                var wordWithBrackets = template.NotificationText.Substring(openBracketIndex, closeBracketIndex - openBracketIndex + 1).ToLower();
+                template.NotificationText = dictionaryParam.ContainsKey(wordWithBrackets.Substring(1, wordWithBrackets.Length - 2)) ?
                      template.NotificationText.
                             Replace(wordWithBrackets,
-                            dictionaryParam[wordWithBrackets.Substring(1, wordWithBrackets.Length - 1)]) :
+                            dictionaryParam[wordWithBrackets.Substring(1, wordWithBrackets.Length - 2)]) :
                      template.NotificationText.
                             Replace(wordWithBrackets, $"{wordWithBrackets.Substring(1, wordWithBrackets.Length - 2)}");
 
